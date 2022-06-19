bdff876dfee52c8bfc0fbbbaeef66f2d4a31282d
Paul Clarke <pauly@clarkeology.com>
2022-06-19 20:09:50 +0000

    🚚 move files in, symlinks did not work

--- scripts/octopus.js
+++ scripts/octopus.js
@@ -1,1 +1,87 @@
-/Users/paulclarke/sites/clarkeology.com/scripts/octopus.js
+#!/usr/bin/env node
+
+/* eslint-disable camelcase */
+
+const graph = require('../src/lib/graph') // graph library I wrote for popex
+
+/* work out my rate etc
+const products = require('../data/products')
+const product = require('../data/product')
+// my deal is E-1R-VAR-22-04-02-J so find it - @todo don't hardcode this
+const key = Object.keys(product.single_register_electricity_tariffs).find(key => product.single_register_electricity_tariffs[key].direct_debit_monthly.code === 'E-1R-VAR-22-04-02-J')
+const { standing_charge_inc_vat: standingCharge, standard_unit_rate_inc_vat: rate } = product.single_register_electricity_tariffs[key].direct_debit_monthly
+// or just hardcode this here...
+*/
+
+const rate = 29.48
+const standingCharge = 41.39
+
+// octopus format is like [{ consumption: 666, interval_start: '2022-06-01T12:00:00Z' }]
+// popex graph format needs only [timestamp, consumption]
+// so use this to map it
+const fromOctopusToPair = ({ consumption, interval_start, interval_end }) => {
+  return [
+    new Date(interval_end).getTime(),
+    consumption
+  ]
+}
+const lastWeek = new Date()
+lastWeek.setDate(lastWeek.getDate() - 7)
+lastWeek.setHours(0, 0, 0, 0)
+const thisWeek = (pair) => pair[0] > lastWeek.getTime()
+const electricity = require('../data/electricity_this_week').results.map(fromOctopusToPair).filter(thisWeek)
+
+const hackyWidthOfLabelPerDay = day => {
+  if (day === 1) return 48 // wider for the M in Mon
+  if (day === 3) return 48 // wider for the W in Wed
+  return 43
+}
+
+const dataSets = [electricity] // popex graph function needs an array of arrays
+const labels = ['Kwh 💡'] // popex graph function wants an optional array of labels
+const description = 'Energy usage graph'
+const border = '#999' // colours
+const colors = ['#9e9', '#99e', '#e99'] // colours
+const width = 800
+const height = 80
+const margin = 15
+const rounding = 1
+const fontSize = '0.6em'
+let sum = 0
+
+// popex graph function takes an optional addComment hook,
+// it is provided these params which we can use to drop a label in
+// at a point on the graph
+function addComment ({ x, y, maxX, maxY, color, label, ts, value }) {
+  sum += value
+  const thisDay = new Date(ts)
+  const endOfDay = (thisDay.getHours() === 23 && thisDay.getMinutes() === 30)
+  if (endOfDay) {
+    const cost = (standingCharge + rate * sum) / 100
+    const top = parseInt(5 + 5 * Math.random(), 10)
+    const width = hackyWidthOfLabelPerDay(thisDay.getDay()) || 48
+    // draw an svg label with a line, this one pointing to the left
+    const comment = `<line x1="${x}" y1="${y}" x2="${x}" y2="${top}" stroke="#ddd" />
+  <rect x="${x - (width - 10)}" y="${top}" width="${width}" height="12" fill="#fff" stroke="#ddd" />
+  <text fill="${color}" x="${x - (width - 13)}" y="${top + 10}">${thisDay.toDateString().substr(0, 3)} £${cost.toFixed(2)}</text>`
+    sum = 0 // reset sum, we only really want the sum per day
+    return comment
+  }
+  const wakeUpTime = (thisDay.getHours() === 7 && thisDay.getMinutes() === 0)
+  if (wakeUpTime) {
+    const cost = (rate * sum) / 100 // no standingCharge this time
+    const top = parseInt(y - 30 - 5 * Math.random(), 10)
+    // draw an svg label with a line, pointing to the right
+    return `<line x1="${x}" y1="${y}" x2="${x}" y2="${top}" stroke="#ddd" />
+  <rect x="${x - 2}" y="${top}" width="40" height="12" fill="#fff" stroke="#ddd" />
+  <text fill="${color}" x="${x}" y="${top + 10}">🕖 £${cost.toFixed(2)}</text>`
+  }
+  // another label for the peak usage
+  if (value === maxY) {
+    if (ts !== maxX) return `<text fill="${color}" x="${x + 10}" y="${y}">⚠️ ${value.toFixed(2)}${label}</text>`
+  }
+  // could add other labels here?
+}
+
+// everything but dataSets is  optional
+console.log(graph({ dataSets, labels, description, width, height, border, margin, colors, rounding, addComment, fontSize }))
--- src/lib/graph.js
+++ src/lib/graph.js
@@ -1,1 +1,128 @@
-/Users/paulclarke/sites/clarkeology.com/src/lib/graph.js
+// graph library I wrote gor popex
+// designed to create some unbloated svg of a graph
+
+const byTimestamp = (a, b) => a[0] - b[0]
+
+const thisYear = new Date().getYear()
+
+const defaultAddComment = ({ x, y, maxX, maxY, color, label, ts, value, comments }) => {
+  if (value === maxY) {
+    if (ts !== maxX) return `<text font-size="smaller" fill="${color}" x="${x + 10}" y="${y}">${label} ${value}</text>`
+  }
+}
+
+module.exports = ({
+  addComment = defaultAddComment,
+  border = '#000',
+  colors = [
+    'red',
+    'orange',
+    'green',
+    'blue',
+    'pink'
+  ],
+  dataSets = [ // should be using typescript here!
+    [
+      // timestamp,
+      // value
+    ]
+  ],
+  fontSize,
+  labels = [],
+  description,
+  rounding = 100,
+  margin = 30,
+  height = 300,
+  width = 900 }) => {
+  const marginTop = margin
+  const marginBottom = margin
+  const marginLeft = margin * 2
+  const marginRight = margin * 4
+
+  const maxHeight = height
+  const maxWidth = width || (maxHeight * 3) // to get image proportions
+  const imageHeight = maxHeight + marginTop + marginBottom
+  const imageWidth = maxWidth + marginLeft + marginRight
+
+  const allChartData = dataSets.reduce((acc, val) => acc.concat(val), []).filter(Boolean)
+
+  const values = Object.values(allChartData).map(value => value[1])
+  const times = Object.values(allChartData).map(value => value[0])
+  const floorX = Math.floor(Math.min.apply(null, times) / rounding) * rounding
+  const maxX = Math.max.apply(null, times)
+  const ceilX = Math.ceil(maxX / rounding) * rounding
+  const floorY = 0 // Math.floor(Math.min.apply(null, values) / rounding) * rounding
+  const maxY = Math.max.apply(null, values)
+  const ceilY = Math.ceil(maxY / rounding) * rounding
+  let value = null
+  let ts = null
+
+  const line = (chartData, color, label) => {
+    if (!chartData) return ''
+    chartData.sort(byTimestamp)
+    let x = 0
+    let y = 0
+    const comments = []
+    const descriptionTag = `<desc>${label} graph line</desc>`
+    const path = '<polyline points="' + Object.values(chartData).map(pair => {
+      [ts, value] = pair
+      x = marginLeft + Math.round((ts - floorX) / (ceilX - floorX) * maxWidth)
+      const height = Math.round((value - floorY) / (ceilY - floorY) * maxHeight)
+      y = maxHeight + marginTop - height
+      const comment = addComment({ x, y, maxX, maxY, color, label, ts, value, comments })
+      if (comment) comments.push(comment)
+      return [x, y]
+    }).filter(Boolean).join(' ') + '" fill="none" />'
+    return `<svg stroke="${color}">\n    ${descriptionTag}\n    ${path}\n  </svg>\n  <text fill="${color}" x="${x}" y="${y}">${label || value}</text>${comments.length ? '\n  ' + comments.filter(Boolean).join('\n  ') : ''}`
+  }
+
+  const priceLine = dataSets.map((chartData, index) => line(chartData, colors[index] || 'black', labels && labels[index])).join('\n  ')
+
+  let axis = `<svg fill="${border}" text-anchor="end">\n    <path d="m ${marginLeft} ${marginTop} v ${maxHeight} h ${maxWidth}" stroke="${border}" fill="transparent" stroke-width="2" />\n`
+  let interval = 5000
+  while (interval >= ceilY) {
+    interval = interval / 10
+  }
+  for (let value = 0; value < ceilY; value += interval) {
+    if (value < floorY) continue
+    const height = Math.round((value - floorY) / (ceilY - floorY) * maxHeight)
+    const y = maxHeight + marginTop - height
+    axis += `    <text x="${marginLeft}" y="${y}">${value} -</text>\n`
+  }
+  const firstDateLabelX = new Date(floorX)
+  firstDateLabelX.setDate(firstDateLabelX.getDate() + 1)
+  firstDateLabelX.setHours(0, 0, 0, 0)
+  ts = firstDateLabelX.getTime()
+  const stepInMonths = (ceilX - floorX) / (1000 * 60 * 60 * 24) > 30
+  const anchor = stepInMonths ? '' : ' text-anchor="start"' // actually wrong anchoring if stepping in months @todo
+  while (ts < ceilX) {
+    const dateX = new Date(ts)
+    if (stepInMonths) {
+      dateX.setMonth(dateX.getMonth() + 1)
+      dateX.setDate(1)
+    } else {
+      dateX.setDate(dateX.getDate() + 1)
+      dateX.setHours(0, 0, 0, 0)
+    }
+    ts = dateX.getTime()
+    const y = maxHeight + marginTop + margin / 2
+    const x = marginLeft + Math.round((ts - floorX) / (ceilX - floorX) * maxWidth)
+    let label = String(new Date(ts)).substr(4, 6)
+    if (stepInMonths) {
+      label = String(new Date(ts)).substr(4, 3)
+    }
+    if (dateX.getYear() !== thisYear) {
+      label = `${label} '${String(dateX.getFullYear()).substr(-2)}`
+    }
+    axis += `    <text x="${x}" y="${y}"${anchor}>${label}</text>\n`
+  }
+  axis += '  </svg>'
+
+  return `
+<svg viewBox="0 0 ${imageWidth} ${imageHeight}" fill="transparent" xmlns="http://www.w3.org/2000/svg"${fontSize ? ` font-size="${fontSize}"` : ''}>
+  <desc>${description || 'popex graph'}</desc>
+  <rect x="${marginLeft}" y="${marginTop}" width="${maxWidth}" height="${maxHeight}" />
+  ${priceLine}
+  ${axis}
+</svg>`
+}
